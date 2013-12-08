ff560fce6f36fbcd9515ad8ce29f0334c7d1c0a3
Sheel Choksi <sheelchoksi@gmail.com>
2013-12-07 22:38:26 -0800

    Clean up controller triplication

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,100 +1,56 @@
 var amassControllers = angular.module("amassControllers", []);
 
-amassControllers.controller('NavBarCtrl', ['$scope', '$location',
-                            function ($scope, $location) {
-                              $scope.isSelected = function (viewLocation) {
-                                return viewLocation === $location.path();
-                              };
-                            }]);
+(function() {
+  amassControllers.controller('NavBarCtrl', ['$scope', '$location',
+                              function ($scope, $location) {
+                                $scope.isSelected = function (viewLocation) {
+                                  return viewLocation === $location.path();
+                                };
+                              }]);
 
-amassControllers.controller('VideographerNewCtrl', ['$scope', '$anchorScroll', 'Videographer',
-                            function ($scope, $anchorScroll, Videographer) {
-                              $anchorScroll();
-                              $scope.videographer = new Videographer();
-                              $scope.invalidField = function(param) {
-                                return $scope.submitted && param;
-                              };
-                              $scope.clear = function() {
-                                $scope.videographer = new Videographer();
-                                $scope.created = false;
-                                $scope.submitted = false;
-                              };
-                              $scope.create = function() {
-                                $scope.submitted = true;
-                                $scope.formError = false;
-                                if ($scope.form.$valid) {
-                                  $scope.saving = true;
-                                  $scope.videographer.$save(
-                                    function() { 
-                                      $scope.saving = false; 
-                                      $scope.created = true;
-                                    },
-                                    function() {
-                                      $scope.saving = false;
-                                      $scope.formError = "There was a problem with your request. Please try again later.";
-                                    }
-                                  );
-                                }
-                              };
-                            }]);
+  function registerSharedControllerMethods($scope, resourceName, Resource) {
+    $scope[resourceName] = new Resource();
+    $scope.invalidField = function(param) {
+      return $scope.submitted && param;
+    };
+    $scope.clear = function() {
+      $scope[resourceName] = new Resource();
+      $scope.created = false;
+      $scope.submitted = false;
+    };
+    $scope.create = function() {
+      $scope.submitted = true;
+      $scope.formError = false;
+      if ($scope.form.$valid) {
+        $scope.saving = true;
+        $scope[resourceName].$save(
+          function() { 
+            $scope.saving = false; 
+            $scope.created = true;
+          },
+          function() {
+            $scope.saving = false;
+            $scope.formError = "There was a problem with your request. Please try again later.";
+          }
+        );
+      }
+    }
+  }
 
-amassControllers.controller('CharityNewCtrl', ['$scope', '$anchorScroll', 'Charity',
-                            function ($scope, $anchorScroll, Charity) {
-                              $anchorScroll();
-                              $scope.charity = new Charity();
-                              $scope.invalidField = function(param) {
-                                return $scope.submitted && param;
-                              };
-                              $scope.clear = function() {
-                                $scope.charity = new Charity();
-                                $scope.created = false;
-                                $scope.submitted = false;
-                              };
-                              $scope.create = function() {
-                                $scope.submitted = true;
-                                $scope.formError = false;
-                                if ($scope.form.$valid) {
-                                  $scope.saving = true;
-                                  $scope.charity.$save(
-                                    function() { 
-                                      $scope.saving = false; 
-                                      $scope.created = true;
-                                    },
-                                    function() {
-                                      $scope.saving = false;
-                                      $scope.formError = "There was a problem with your request. Please try again later.";
-                                    }
-                                  );
-                                }
-                              };
-                            }]);
+  amassControllers.controller('VideographerNewCtrl', ['$scope', '$anchorScroll', 'Videographer',
+                              function ($scope, $anchorScroll, Videographer) {
+                                $anchorScroll();
+                                registerSharedControllerMethods($scope, 'videographer', Videographer);
+                              }]);
 
-amassControllers.controller('FollowCtrl', ['$scope', 'Follower',
-                            function ($scope, Follower) {
-                              $scope.follower = new Follower();
-                              $scope.invalidField = function(param) {
-                                return $scope.submitted && param;
-                              };
-                              $scope.clear = function() {
-                                $scope.follower = new Follower();
-                                $scope.created = false;
-                                $scope.submitted = false;
-                              };
-                              $scope.create = function() {
-                                $scope.submitted = true;
-                                $scope.formError = false;
-                                if ($scope.form.$valid) {
-                                  $scope.saving = true;
-                                  $scope.follower.$save(
-                                    function() { 
-                                      $scope.saving = false; 
-                                      $scope.created = true;
-                                    },
-                                    function() {
-                                      $scope.saving = false;
-                                      $scope.formError = "There was a problem with your request. Please try again later.";
-                                    }
-                                  );
-                                }
-                              };
-                            }]);
+  amassControllers.controller('CharityNewCtrl', ['$scope', '$anchorScroll', 'Charity',
+                              function ($scope, $anchorScroll, Charity) {
+                                $anchorScroll();
+                                registerSharedControllerMethods($scope, 'charity', Charity);
+                              }]);
+
+  amassControllers.controller('FollowCtrl', ['$scope', 'Follower',
+                              function ($scope, Follower) {
+                                registerSharedControllerMethods($scope, 'follower', Follower);
+                              }]);
+})();
