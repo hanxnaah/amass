85ae5448d40d2f3c1355f6c4af11c73c85b52b58
Sheel Choksi <sheelchoksi@gmail.com>
2014-01-23 21:54:16 -0800

    Change some copy and remove about page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,8 +8,6 @@ amassApp.config(['$routeProvider',
                 function($routeProvider) {
                   $routeProvider.when("/", {
                     templateUrl: "../home.html"
-                  }).when("/about", {
-                    templateUrl: "../about.html"
                   }).when("/filmmaker/new", {
                     templateUrl: "../filmmaker/new.html",
                     controller: 'FilmmakerNewCtrl'
