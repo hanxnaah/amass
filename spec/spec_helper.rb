8865712e31bca6c1c5f04a82e569285fea218be6
Holman Gao <holman@golmansax.com>
2015-05-15 15:53:12 -0700

    Test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,7 @@ if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
   SimpleCov.coverage_dir(dir)
 end
+SimpleCov.minimum_coverage 100
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
