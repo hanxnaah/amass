05ce21dfcfefb652e77a25565d04ba176b6f1568
Holman Gao <holman@golmansax.com>
2015-05-15 16:00:00 -0700

    Use 90% coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
   SimpleCov.coverage_dir(dir)
 end
-SimpleCov.minimum_coverage 100
+SimpleCov.minimum_coverage 90
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
