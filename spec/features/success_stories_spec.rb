1e98cc310ab22d7ea2baa627e1d8e4d892aff33e
Holman Gao <holman@golmansax.com>
2015-05-16 09:13:15 -0700

    Oops

---
+++ spec/features/success_stories_spec.rb
@@ -0,0 +1,17 @@
+feature 'success stories flow' do
+  scenario 'scroll through success stories' do
+    visit '/'
+    expect(page).to have_content('Success Stories')
+
+    expect(page).to have_content('Community Yoga Project')
+    expect(page).to have_no_content('Labor Link')
+
+    find('.slick-next').click
+    expect(page).to have_content('Labor Link')
+    expect(page).to have_no_content('Community Yoga Project')
+
+    within_frame 'vimeo-116570432' do
+      expect(page).to have_css('#player')
+    end
+  end
+end
