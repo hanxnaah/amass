3611c7ff2b344c19bd7ae23df11e8e50cc7a02cc
Holman Gao <holman@golmansax.com>
2015-05-25 21:46:19 -0700

    Add test for 404

--- spec/features/error_handling_spec.rb
+++ spec/features/error_handling_spec.rb
@@ -3,4 +3,10 @@ feature 'error handling flow' do
     visit '/crap'
     expect(page).to have_content("doesn't exist.")
   end
+
+  it 'returns a 404 when user goes to bad page' do
+    Capybara.current_driver = :rack_test
+    visit '/crap'
+    expect(page.status_code).to eq(404)
+  end
 end
