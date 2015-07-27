3cf800e6d35291968342c9dbafb9ef4b3873a8de
Holman Gao <holman@golmansax.com>
2015-07-26 20:41:11 -0700

    Remove the clear button

--- app/assets/javascripts/organization_new/ctrl.js
+++ app/assets/javascripts/organization_new/ctrl.js
@@ -6,13 +6,6 @@
   amassControllers.controller('OrganizationNewCtrl', [
     '$scope', '$anchorScroll', 'Organization', '$analytics',
     function ($scope, $anchorScroll, Organization, $analytics) {
-      $scope.resetResource = function () {
-        $scope.organization = new Organization();
-        $scope.organization.payment_includes_expenses = true;
-        $scope.organization.deadline = new Date();
-        $scope.organization.payment = 0;
-      };
-
       /* global window */
       window.registerSharedControllerMethods(
         $scope,
--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -5,40 +5,30 @@
   window.registerSharedControllerMethods = function ($scope, resourceName,
       Resource, $analytics) {
 
-    if (!$scope.resetResource) {
-      $scope.resetResource = function () {
-        $scope[resourceName] = new Resource();
-      };
-    }
-    $scope.resetResource();
+    $scope[resourceName] = new Resource();
 
     $scope.invalidField = function (param) {
       return $scope.submitted && param;
     };
-    $scope.clear = function () {
-      $scope.resetResource();
-      $scope.created = false;
-      $scope.submitted = false;
-    };
+
     $scope.create = function () {
       $scope.submitted = true;
       $scope.formError = false;
       if ($scope.form.$valid) {
         $scope.saving = true;
-        $scope[resourceName].$save(
-          function () {
+        $scope[resourceName].$save()
+          .then(function (resource) {
             $scope.saving = false;
             $scope.created = true;
 
             $analytics.eventTrack('Sign-up', { type: resourceName });
-            $scope.resetResource();
-          },
-          function () {
+            $scope[resourceName] = resource;
+          })
+          .catch(function () {
             $scope.saving = false;
             $scope.formError = 'There was a problem with your request. ' +
               'Please try again later.';
-          }
-        );
+          })
       }
     };
   };
