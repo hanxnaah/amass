e8e067a8ff0c9cb3a9ba283d92dfb962561799e4
yangz <eforblue@gmail.com>
2015-10-29 12:20:25 -0700

    pricing page fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def index
     gon.success_stories = t('success_stories')
     gon.team_bios = t('team_bios')
+    gon.faqs = t('faqs')
     render layout: false
   end
 end
