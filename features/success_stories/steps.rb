b29ffe964f59b2373cc4373b305dcafed7be3e07
Holman Gao <holman@golmansax.com>
2015-11-13 14:40:51 -0800

    Update gems

--- features/success_stories/steps.rb
+++ features/success_stories/steps.rb
@@ -4,7 +4,9 @@ When 'I switch to next success story' do
 
   expect(page).to have_content('Community Yoga Project')
   expect(page).to have_no_content('Labor Link')
-  find('.slick-next').click
+
+  # Need to use JS because Selenium clicks on the header above this button
+  page.evaluate_script("$('.slick-next').click()")
 end
 
 Then 'I see a different success story' do
