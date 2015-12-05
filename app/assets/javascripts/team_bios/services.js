c91bfcd4f368ea5ec27fd1fd4efd2201a5e078cd
Holman Gao <holman@golmansax.com>
2015-12-05 12:39:37 -0800

    Fix slug on teams page

--- app/assets/javascripts/team_bios/services.js
+++ app/assets/javascripts/team_bios/services.js
@@ -6,10 +6,6 @@
   services.factory('TeamBioStore', ['gon', function (gon) {
     var teamBios = angular.copy(gon.teamBios);
 
-    angular.forEach(teamBios, function (teamBio) {
-      teamBio.slug = teamBio.nickname.toLowerCase();
-    });
-
     return {
       getAll: function () {
         return teamBios;
